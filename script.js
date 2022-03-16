2512d0aa4fdaad22abcd22d467238726d4a1d478
sizwe <nkosiczwe@gmail.com>
2022-03-16 14:31:58 +0200

    Add variables

---
+++ script.js
@@ -0,0 +1,10 @@
+//Declare and initialise variables
+let heads = 0;
+let tails = 0;
+
+//DOM variables
+let coin = document.querySelector('.coin');
+let flipBtn = document.querySelector('#flip-button');
+let resetBtn = document.querySelector('#reset-button');
+
+console.log(flipBtn, resetBtn);
